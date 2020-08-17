1185938bb679ef653d440dd1ce159cf5ea9c9abd
Ellie Wetzel <elliehw2005@gmail.com>
2020-08-16 17:25:42 -0700

    ExceptionsDemo, CustomException (IP)

--- src/_01_introduction/ExceptionsDemo.java
+++ src/_01_introduction/ExceptionsDemo.java
@@ -2,6 +2,7 @@ package _01_introduction;
 
 public class ExceptionsDemo {
 	public static void main(String[] args) {
+		ExceptionsDemo ed = new ExceptionsDemo();
 		// Exceptions are ways to allow your program to continue running 
 		// in the event of unexpected behavior or input.
 		// All exceptions extend from Java's Exception class.
@@ -15,13 +16,25 @@ public class ExceptionsDemo {
 		//    call to testMethod1 with a try/catch block.
 		//    In the catch block, put a print statement so you know
 		//    if the code is being ran.
-		
+		try {
+			testMethod1(3);
+		} catch (Exception e) {
+			System.out.println("to");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// 2. Now call testMethod1 with a value greater than 5. 
 		//    Running your program should not print the statement
 		//    in the catch block.
 		//    You can use the Exception class's printStackTrace() method
 		//    to print a list of methods that are currently in scope.
-		
+		try {
+			testMethod1(7);
+		} catch (Exception e) {
+			System.out.println("hi");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// 3. Create a new class called CustomException that extends Exception.
 		//    Add a void method called terminate that simply calls System.exit(0);
 		
@@ -30,6 +43,13 @@ public class ExceptionsDemo {
 		//    CustomException object.
 		//    This demonstrates how custom exception classes can be used to
 		//    handle exceptions in unlimited ways.
+		try {
+			testMethod2(-5);
+		} catch (CustomException e) {
+			terminate(); // fix
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public static void testMethod1(int x) throws Exception {
@@ -40,4 +60,9 @@ public class ExceptionsDemo {
 	
 	// 4. Create a static void method called testMethod2(int x) that throws
 	//    a CustomException if x is negative.
+	static void testMethod2(int x) throws CustomException {
+		if(x < 0) {
+			throw new CustomException();
+		}
+	}
 }
